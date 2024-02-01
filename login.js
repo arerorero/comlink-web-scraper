19196640a57ffff5fd2a2862ae5e5934a0f83f7f
Filippo Grassano <com.filippo@outlook.com>
2024-02-01 16:36:11 -0300

    login info is now hidden

--- login.js
+++ login.js
@@ -1,9 +1,14 @@
+const fs = require("fs");
+
 module.exports.login = async function login(page) {
+  let login = fs.readFileSync("login.json", "utf8");
+  login = await JSON.parse(login);
+
   await page.click("a.showModal.login");
 
-  await page.type('input[name="txtCodigo"]', "6316617");
-  await page.type('input[name="txtUsuario"]', "wilson.sartori");
-  await page.type('input[name="txtSenha"]', "CIARAMA2022");
+  await page.type('input[name="txtCodigo"]', login.code);
+  await page.type('input[name="txtUsuario"]', login.login);
+  await page.type('input[name="txtSenha"]', login.password);
 
   await page.evaluate(() => {
     const buttons = document.querySelectorAll('input[type="submit"]');
--- scraper.js
+++ scraper.js
@@ -1,30 +1,9 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 const { login } = require("./login");
 
 const fs = require("fs");
 
 const url = "http://comlink.com.br";
-
-// async function login(page) {
-//   await page.click("a.showModal.login");
-
-//   await page.type('input[name="txtCodigo"]', "6316617");
-//   await page.type('input[name="txtUsuario"]', "wilson.sartori");
-//   await page.type('input[name="txtSenha"]', "CIARAMA2022");
-
-//   await page.evaluate(() => {
-//     const buttons = document.querySelectorAll('input[type="submit"]');
-//     for (const button of buttons) {
-//       if (button.value === "Login") {
-//         button.click();
-//       }
-//     }
-//   });
-
-//   await new Promise((r) => setTimeout(r, 2000));
-// }
 
 async function collect1(page) {
   const data = await page.evaluate(() => {
